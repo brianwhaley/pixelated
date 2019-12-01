1a4b0f089ab0ed256c382413c0726830faa7b13e
Brian Whaley <brian.whaley@gmail.com>
2019-12-01 11:47:10 -0500

    Pixelated - new sitemap files

--- src/menu.js
+++ src/menu.js
@@ -5,14 +5,14 @@ class Menu extends Component {
         return (
 
 		<ul class="grid12 clearfix">
-                    <li><a href="./index.html">Home</a></li>
-                    <li><a href="./gallery.html?tag=portfolio-all">Work Portfolio</a></li>
-                    <li><a href="./gallery.html?tag=pixelatedviewsgallery">Photo Gallery</a></li>
-                    <li><a href="http://blog.pixelatedviews.com">Pixelated Views Blog</a></li>
-                    <li><a href="./socialmedia.html">Social Media</a></li>
-                    <li><a href="./photography.html">Photography</a></li>
-                    <li><a href="./recipes.html">Recipes</a></li>
-                </ul>			
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="./gallery.html?tag=portfolio-all">Work Portfolio</a></li>
+            <li><a href="./gallery.html?tag=pixelatedviewsgallery">Photo Gallery</a></li>
+            <li><a href="http://blog.pixelated.tech">Pixelated Views Blog</a></li>
+            <li><a href="./socialmedia.html">Social Media</a></li>
+            <li><a href="./photography.html">Photography</a></li>
+            <li><a href="./recipes.html">Recipes</a></li>
+        </ul>			
 
         );
     }
