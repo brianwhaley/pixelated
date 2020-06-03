e9f0b2ea4a8c072909796a139755d1431163d149
Brian Whaley <brian.whaley@gmail.com>
2020-06-03 17:19:41 -0400

    Pixelated - Dev - 2020-05-16 - stkr update, tech-jokes stubs

--- src/pages/nerdjokes.js
+++ src/pages/nerdjokes.js
@@ -13,7 +13,7 @@ export default class NerdJokes extends Component {
 							<div className="callout-body grid12">
 								<div className="grid12">
 									<div className=" grid12 centered">
-										<a href="https://slack.com/oauth/v2/authorize?client_id=1058085085824.1058509925568&scope=chat:write,commands,files:read,im:write">
+										<a href="https://slack.com/oauth/v2/authorize?client_id=1058085085824.1093097617364&scope=chat:write,chat:write.public,commands,im:write">
 											<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" 
 												srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
 										<br />
