b3be8cc90c1bd8f732a6000509e76feadb38e009
Brian Whaley <brian.whaley@gmail.com>
2022-09-04 22:47:16 -0400

    recipe

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ export * from './components/pixelated.callout';
 // export * from './components/pixelated.menu-accordion';
 // export * from './components/pixelated.menu-simple';
 // export * from './components/pixelated.nerdjoke';
-// export * from './components/pixelated.recipe';
+export * from './components/pixelated.recipe';
 export * from './components/pixelated.resume';
 // export * from './components/pixelated.socialcard';
