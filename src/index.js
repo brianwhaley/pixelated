717f1713bc5886b421e98a0873d22fe7d444ed17
Brian Whaley <brian.whaley@gmail.com>
2022-09-04 23:24:20 -0400

    carousel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ export * from './components/pixelated.callout';
 export * from './components/pixelated.carousel';
 export * from './components/pixelated.functions';
 export * from './components/pixelated.menu-accordion';
-export * from './components/pixelated.menu-simple';
+// export * from './components/pixelated.menu-simple';
 export * from './components/pixelated.nerdjoke';
 export * from './components/pixelated.recipe';
 export * from './components/pixelated.resume';
