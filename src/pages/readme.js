1190c322e07a2e7edfea21a87d95173f9412f5be
Brian Whaley <brian.whaley@gmail.com>
2023-10-11 22:31:30 -0400

    readme fix #3 (maybe)

--- src/pages/readme.js
+++ src/pages/readme.js
@@ -18,17 +18,18 @@ export default class Readme extends Component {
 	}
 	*/
 	async componentDidMount() {
-        const file = await import('../data/readme.md');
+		console.log("ComponentDidMount");
+		const file = await import('../data/readme.md');
 		console.log(file);
-        const response = await fetch(file.default);
+		const response = await fetch(file.default);
 		console.log(response);
-        const text = await response.text();
+		const text = await response.text();
 		console.log(text);
-        this.setState({
-            readmeText: text
-        })
+		this.setState({
+			readmeText: text
+		})
 		console.log(this.state.readmeText);
-    }
+	}
 	render() {
 		return (
 			<div className="section-container">
