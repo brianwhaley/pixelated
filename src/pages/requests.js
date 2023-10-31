6a19dbe7c1469229a07eeafa35f3f1636cc0591c
Brian Whaley <brian.whaley@gmail.com>
2023-10-31 16:04:23 -0400

    custom sunglasses page, requests page, refactor routing

--- src/pages/requests.js
+++ src/pages/requests.js
@@ -17,6 +17,7 @@ export default class Requests extends Component {
 		}
 		this.state.tableData = requestData;
 		this.showDialog = this.showDialog.bind(this);
+		this.saveDialog = this.saveDialog.bind(this);
 		this.closeDialog = this.closeDialog.bind(this);
 		this.addRow = this.addRow.bind(this);
 	}
@@ -24,20 +25,59 @@ export default class Requests extends Component {
 		const mydialog = document.getElementById("newRequestDialog");
 		mydialog.showModal();
 	}
-	closeDialog(e) {
+	saveRowDialog(e) {
 		const mydialog = document.getElementById("newRequestDialog");
 		const myform = document.getElementById("newRequestForm");
-		if(e.target.innerText == "Save") {
-			e.preventDefault();
-			let myFormData = [];
-			const formData = new FormData(myform);
-			for (let [key, value] of formData.entries()) {
-				myFormData[key] = value ;
-			}
-			myFormData["Date"] = new Date().toLocaleDateString() ;
-			myFormData["Status"] = "Submitted" ;
-			this.addRow(myFormData);
+		e.preventDefault();
+		let myFormData = [];
+		const formData = new FormData(myform);
+		for (let [key, value] of formData.entries()) {
+			myFormData[key] = value ;
 		}
+		myFormData["Date"] = new Date().toLocaleDateString() ;
+		myFormData["Status"] = "Submitted" ;
+		console.log(myFormData);
+		this.addRow(myFormData);
+		mydialog.close();
+	}
+	async saveDialog(e){
+		const sendmail_api = "https://nlbqdrixmj.execute-api.us-east-2.amazonaws.com/default/sendmail";
+
+		const mydialog = document.getElementById("newRequestDialog");
+		const tyDialog = document.getElementById("thankYouDialog");
+		const myform = document.getElementById("newRequestForm");
+		e.preventDefault();
+		let myFormData = {};
+		const formData = new FormData(myform);
+		for (let [key, value] of formData.entries()) {
+			myFormData[key] = value ;
+		}
+		myFormData.Date = new Date().toLocaleDateString() ;
+		myFormData.Status = "Submitted" ;
+
+		await fetch(sendmail_api, {
+			method: 'POST',
+			mode: 'cors', 
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(myFormData),
+		})
+			.then((response) => {
+				if (response.status !== 200) {
+					throw new Error(response.statusText);
+				}
+				console.log(response);
+				return response.json();
+			}) 
+
+		mydialog.close();
+		tyDialog.showModal();
+
+	}
+	closeDialog(id) {
+		const mydialog = document.getElementById(id);
 		mydialog.close();
 	}
 	addRow(newRow) {
@@ -49,12 +89,25 @@ export default class Requests extends Component {
 		return (
 			<div className="section-container">
 				<CalloutHeader title="Custom Sunglass Requests" />
-				<Table data={requestData} />
-				<button type="button" id="showDialog" onClick={this.showDialog}>Add</button>
+				<button type="button" id="showDialog" onClick={this.showDialog}>Request a Custom</button>
+				<br /><br />
 				<dialog id="newRequestDialog">
 					<FormEngine name="newrequest" id="newRequestForm" formdata={formData} />
-					<button type="button" id="saveDialog" onClick={this.closeDialog}>Save</button>
-					<button type="button" id="closeDialog" onClick={this.closeDialog}>Close</button>
+					<br />
+					<center>
+						<br />
+						<button type="button" id="saveDialog" onClick={this.saveDialog}>Send</button>
+						<button type="button" id="closeDialog" onClick={() => this.closeDialog('newRequestDialog')}>Close</button>
+					</center>
+				</dialog>
+				<Table data={requestData} />
+				<dialog id="thankYouDialog">
+					<CalloutHeader title="Thank you!" />
+					<center>Thank you for your your request.  Your request data has been sent for review.</center>
+					<br />
+					<center>
+						<br /><button type="button" id="closeDialog" onClick={() => this.closeDialog('thankYouDialog')}>Close</button>
+					</center>
 				</dialog>
 			</div>
 		);
