b6b5cdb19ebe349c80db1d3f883086a29096e2e5
Brian Whaley <brian.whaley@gmail.com>
2025-04-18 08:41:03 -0400

    1.2.2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,11 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 
+import path from 'path'; 
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const config = {
   core: {
     builder: {
@@ -7,12 +13,15 @@ const config = {
       options: { fsCache: false } 
     },
   },
-  framework:"@storybook/react-webpack5",
-  stories: [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  staticDirs: [ /* "../dist", */ "../src" ],
+  stories: [ "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)" ],
 
   webpackFinal: async (config) => {
+
     // WEBPACK FOR JS FILES
     config.module.rules.push({
       test: /\.(js|jsx)$/,
@@ -28,6 +37,20 @@ const config = {
       },
     });
     config.resolve.extensions.push('.js', '.jsx');
+
+    // WEBPACK FOR CSS FILES
+    /* config.module.rules.push({
+      test: /\.(css)$/,
+      use: {
+        loader: "css-loader",
+        options: {
+          url: false,
+          esModule: false
+        }
+      }
+    })
+    config.resolve.extensions.push('.css'); */
+
     // WEBPACK FOR LESS FILES
     config.module.rules.push({
       test: /\.(less)$/,
@@ -37,7 +60,27 @@ const config = {
           { loader: 'less-loader', options: { lessOptions: { javascriptEnabled: true } } },
       ]
     })
-    config.resolve.extensions.push('.less');
+    config.resolve.extensions.push('.less'); 
+
+    // WEBPACK FOR IMAGE FILES
+    /* config.module.rules.push({
+      test: /\.(png|jpg|jpeg|gif|svg)$/,
+      type: 'asset/resource',
+      use: {
+        loader: 'url-loader',
+        options: {
+            name: '[name].[ext]',
+            outputPath: '../dist/images', // Output images to an 'images' folder in the Storybook build
+            publicPath: '/images/', // Public URL path to access images
+        },
+      },
+    }); */
+
+    // ALIAS FOR ABSOLUTE PATH FOR IMAGES
+    config.resolve.alias = {
+      "/images": path.resolve(__dirname, "../dist/images"),
+      "images": path.resolve(__dirname, "../dist/images"),
+    };
 
     return config;
   },
