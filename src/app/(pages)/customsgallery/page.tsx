ad0cb0d06bacf2447b13ff935cb4107e47ac72cd
Brian Whaley <brian.whaley@gmail.com>
2025-06-05 23:35:32 -0400

    update carousels to new component

--- src/app/(pages)/customsgallery/page.tsx
+++ src/app/(pages)/customsgallery/page.tsx
@@ -1,26 +1,60 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PageHeader } from "@/app/components/general/pixelated.general";
 import { Carousel } from "@brianwhaley/pixelated-components";
-
-const tags = {tag: "customsunglasses"};
-
-function GalleryWrapper( props: { props: { tag: string; }; } ) {
-	const myTag = props.props.tag;
-	const flickrProps = { urlProps: { tags: myTag, photoSize: "Large" } };
-	return (
-		<div className="section-container">
-			<Carousel flickr={flickrProps} type="slider" />
-		</div>
-	);
-}
+import { GetFlickrData, GenerateFlickrCards } from '@brianwhaley/pixelated-components';
 
 export default function Gallery () {
+
+	const [ flickrCards, setFlickrCards ] = useState([]);
+
+	useEffect(() => {
+		async function getFlickrCards() {
+			const myPromise = GetFlickrData({
+				flickr : {
+					baseURL: 'https://api.flickr.com/services/rest/?',
+					urlProps: {
+						method: 'flickr.photos.search',
+						api_key: '882cab5548d53c9e6b5fb24d59cc321d',
+						user_id: '15473210@N04',
+						tags: 'customsunglasses',
+						extras: 'date_taken,description,owner_name',
+						sort: 'date-taken-desc',
+						per_page: 500,
+						format: 'json',
+						photoSize: 'Large',
+						nojsoncallback: 'true' /*,
+						startPos: 0 */
+					}
+				} 
+			});
+			const myFlickrImages = await myPromise;
+			const myFlickrCards = GenerateFlickrCards({flickrImages: myFlickrImages, photoSize: 'Medium'});
+			// REMOVE LINKS
+			const myScrubbedFlickrCards = myFlickrCards.map((obj: any) => {
+				delete obj.link;
+				delete obj.headerText;
+				delete obj.bodyText;
+				return obj;
+				});
+			setFlickrCards(myScrubbedFlickrCards);
+		}
+		getFlickrCards();
+	}, []); // Empty dependency array to run only once
+
+
 	return (
 		<>
-			<PageHeader title="Custom Sunglasses Gallery" />
-			<GalleryWrapper props={tags}/>
+			<section id="customs-section">
+				<div className="section-container">
+					<PageHeader title="Custom Sunglasses Gallery" />
+					<Carousel 
+						cards={flickrCards} 
+						draggable={false}
+						imgFit="contain" />
+				</div>
+			</section>
 		</>
 	);
 }
--- src/app/elements/hero.tsx
+++ src/app/elements/hero.tsx
@@ -1,12 +1,55 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Carousel } from "@brianwhaley/pixelated-components";
+import { GetFlickrData, GenerateFlickrCards } from '@brianwhaley/pixelated-components';
+import './hero.css';
+
 
 export default function Hero() {
+
+	const [flickrCards, setFlickrCards] = useState([]);
+
+	useEffect(() => {
+		async function getFlickrCards() {
+			const myPromise = GetFlickrData({
+				flickr : {
+					baseURL: 'https://api.flickr.com/services/rest/?',
+					urlProps: {
+						method: 'flickr.photos.search',
+						api_key: '882cab5548d53c9e6b5fb24d59cc321d',
+						user_id: '15473210@N04',
+						tags: 'pixelatedviewsgallery',
+						extras: 'date_taken,description,owner_name',
+						sort: 'date-taken-desc',
+						per_page: 500,
+						format: 'json',
+						photoSize: 'Large',
+						nojsoncallback: 'true' /*,
+						startPos: 0 */
+					}
+				} 
+			});
+			const myFlickrImages = await myPromise;
+			const myFlickrCards = GenerateFlickrCards({flickrImages: myFlickrImages, photoSize: 'Medium'});
+			// REMOVE LINKS
+			const myScrubbedFlickrCards = myFlickrCards.map((obj: any) => {
+				delete obj.link;
+				delete obj.bodyText;
+				return obj;
+				});
+			setFlickrCards(myScrubbedFlickrCards);
+		}
+		getFlickrCards();
+	}, []); // Empty dependency array to run only once
+
+
 	return (
-		<div id="page-hero" className="row-12col">
-			<Carousel type='hero' />
+		<div id="page-hero">
+			<Carousel 
+				cards={flickrCards} 
+				draggable={false}
+				imgFit="cover" />
 		</div>
 	);
 }
