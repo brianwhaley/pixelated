9a5c4e25bcc1338a6dbfa11c5c2fecb4a50d8d2e
Brian Whaley <brian.whaley@gmail.com>
2025-06-10 18:17:48 -0400

    fix for broken links in social cards affecting site search indexing

--- docs/doc.md
+++ docs/doc.md
@@ -52,7 +52,7 @@ npm version minor
 eslint --fix
 npm version patch --force
 git add * -v
-git commit -m "ga tag fix"
+git commit -m "fix for broken links in social cards affecting site search indexing"
 git push pixelated dev --tags
 git push pixelated dev:main
 
--- next.config.ts
+++ next.config.ts
@@ -13,14 +13,16 @@ const nextConfig: NextConfig = {
 
 	async redirects() {
 		return [
-			{ source: '/index.html', destination: '/', permanent: true, },
-			{ source: '/joke', destination: '/nerdjokes', permanent: true, },
-			{ source: '/gallery.html', has: [ { type: 'query', key: 'tag', value: 'pixelatedviewsgallery' } ], destination: '/photogallery', permanent: true, },
 			{ source: '/customsunglasses.html', destination: '/customsunglasses', permanent: true, },
 			{ source: '/gallery.html', has: [ { type: 'query', key: 'tag', value: 'customsunglasses' } ], destination: '/customsunglasses', permanent: true, },
-			{ source: '/photogallery.html', destination: '/photogallery', permanent: true, },
+			{ source: '/gallery.html', has: [ { type: 'query', key: 'tag', value: 'pixelatedviewsgallery' } ], destination: '/photogallery', permanent: true, },
+			{ source: '/index.html', destination: '/', permanent: true, },
+			{ source: '/joke', destination: '/nerdjokes', permanent: true, },
 			{ source: '/mycustoms.html', destination: '/mycustoms', permanent: true, },
+			{ source: '/photogallery.html', destination: '/photogallery', permanent: true, },
 			{ source: '/photography.html', destination: '/photography', permanent: true, },
+			{ source: '/recipes.html', destination: '/recipes', permanent: true, },
+			{ source: '/requests.html', destination: '/requests', permanent: true, },
 		];
 	},
 
