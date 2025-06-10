f5ffb5234d30d2af8ef33df5a45200b4033ee24c
Brian Whaley <brian.whaley@gmail.com>
2025-06-10 16:53:22 -0400

    fix for google analytics and google tag manager

--- src/components/google/pixelated.googleanalytics.tsx
+++ src/components/google/pixelated.googleanalytics.tsx
@@ -4,29 +4,51 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 declare global {
-    interface Window {
-        dataLayer: any[];
-    }
+	interface Window {
+		dataLayer?: any[];
+		gtag?: (...args: any[]) => void;
+		google_tag_manager?: any;
+	}
 }
 
 export function Analytics(props: { id: string }) {
-	if(typeof document !== 'undefined'){
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const ganalytics = (function () {
-			// INSTALL GA SCRIPT
-			const ga = document.createElement("script");
-			ga.type = "text/javascript";
-			ga.async = true;
-			ga.src = (document.location.protocol === "https:" ? "https:" : "http:") + "//www.googletagmanager.com/gtag/js?id=" + props.id;
-			const s = document.getElementsByTagName("script")[0];
-			s.parentNode?.insertBefore(ga, s);
-			// ADD GA TAG TO PAGE
-			window.dataLayer = window.dataLayer || [];
-			function gtag(...args: any[]) { window.dataLayer.push(args); }
-	        gtag("js", new Date());
-	        gtag("config", props.id);
+
+	function isGA() {
+		const hasGtag = typeof window.gtag === 'function';
+		const hasGTM = typeof window.google_tag_manager !== 'undefined';
+		const hasDataLayer = typeof window.dataLayer !== 'undefined' && Array.isArray(window.dataLayer);
+		const hasScript = (() => {
+			const scripts = document.getElementsByTagName('script');
+			for (let i = 0; i < scripts.length; i++) {
+				if (scripts[i].src && scripts[i].src.includes('googletagmanager.com/gtag/js')) {
+					return true;
+				}
+			}
+			return false;
 		})();
+		return hasGtag || hasGTM || hasDataLayer || hasScript;
 	}
+
+	if(typeof document !== 'undefined'){
+		if(!isGA()){
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const ganalytics = (function () {
+				// INSTALL GA SCRIPT
+				const ga = document.createElement("script");
+				ga.type = "text/javascript";
+				ga.async = true;
+				ga.src = (document.location.protocol === "https:" ? "https:" : "http:") + "//www.googletagmanager.com/gtag/js?id=" + props.id;
+				const s = document.getElementsByTagName("script")[0];
+				s.parentNode?.insertBefore(ga, s);
+				// ADD GA TAG TO PAGE
+				window.dataLayer = window.dataLayer || [];
+				function gtag(...args: any[]) { (window.dataLayer)?.push(args); }
+				gtag("js", new Date());
+				gtag("config", props.id);
+			})();
+		}
+	}
+	
 	return (
 		<div className="ga" suppressHydrationWarning />
 	);
