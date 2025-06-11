9c9c64a574898098b5e8100ff603156a19ffb930
Brian Whaley <brian.whaley@gmail.com>
2025-06-11 09:24:50 -0400

    web imgs for contentful

--- next.config.ts
+++ next.config.ts
@@ -13,16 +13,23 @@ const nextConfig: NextConfig = {
 
 	async redirects() {
 		return [
+			{ source: '/customsgallery.html', destination: '/customsgallery', permanent: true, },
 			{ source: '/customsunglasses.html', destination: '/customsunglasses', permanent: true, },
 			{ source: '/gallery.html', has: [ { type: 'query', key: 'tag', value: 'customsunglasses' } ], destination: '/customsunglasses', permanent: true, },
 			{ source: '/gallery.html', has: [ { type: 'query', key: 'tag', value: 'pixelatedviewsgallery' } ], destination: '/photogallery', permanent: true, },
 			{ source: '/index.html', destination: '/', permanent: true, },
 			{ source: '/joke', destination: '/nerdjokes', permanent: true, },
 			{ source: '/mycustoms.html', destination: '/mycustoms', permanent: true, },
+			{ source: '/nerdjokes.html', destination: '/nerdjokes', permanent: true, },
 			{ source: '/photogallery.html', destination: '/photogallery', permanent: true, },
 			{ source: '/photography.html', destination: '/photography', permanent: true, },
+			{ source: '/readme.html', destination: '/readme', permanent: true, },
 			{ source: '/recipes.html', destination: '/recipes', permanent: true, },
 			{ source: '/requests.html', destination: '/requests', permanent: true, },
+			{ source: '/resume.html', destination: '/resume', permanent: true, },
+			{ source: '/socialmedia.html', destination: '/socialmedia', permanent: true, },
+			{ source: '/stkr.html', destination: '/stkr', permanent: true, },
+			{ source: '/workportfolio.html', destination: '/workportfolio', permanent: true, },
 		];
 	},
 
