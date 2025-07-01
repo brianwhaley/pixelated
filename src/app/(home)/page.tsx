b4e4e43d0693253f25a028254e440f923797914d
Brian Whaley <brian.whaley@gmail.com>
2025-06-30 22:20:02 -0400

    remove sunglass and photography pages

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,60 +1,55 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PageHeader } from "@/app/components/general/pixelated.general";
 import { Callout, CalloutHeader } from "@brianwhaley/pixelated-components";
 import SocialTags from "@/app/elements/socialtags";
+import { Carousel } from "@brianwhaley/pixelated-components";
+import GalleryWrapper from "@/app/elements/gallerywrapper";
+import type { CarouselCardType } from "@brianwhaley/pixelated-components";
 
 export default function Home() {
+
+
+
+	const [ flickrCards, setFlickrCards ] = useState<CarouselCardType[]>([]);
+	const props = { 
+		tags: "", // "workportfolio"
+		method: "flickr.photosets.getPhotos", 
+		photoset_id: "72177720326903710",
+		photoSize: "Large", 
+		callback: setFlickrCards 
+	};
+	useEffect(() => {
+		async function fetchGallery() {
+			await GalleryWrapper(props);
+		}
+		fetchGallery();
+	}, []); 
+
+
 	return (
 		<>
 			<section id="products-section">
 				<div className="section-container">
 					<br />
 					<PageHeader title="Pixelated Products & Services" />
-					<div className="row-12col">
-						<div className="grid-s3-e8">
-							Pixelated offers a wide variety of products and services, 
-							including custom web development for small businesses, 
-							refurbished and customized sunglasses, stock photography, 
-							touchup services, home design and remodeling, and more. 
-							Explore our detailed offerings below. 
-						</div>
-					</div>
-					<div className="row-3col">
+					<div className="row-1col">
 						<Callout
 							url="/workoverview" 
 							img='images/circuitboard.jpg'
 							title='Web Development'
-							content='Passionate Web Technologist,
-								Digital Transformation Professional, User Experience Champion.
-								I get excited watching companies and products grow and thrive.'
-							layout='vertical' />
-						<Callout
-							url='/customsunglasses' 
-							img='/images/customs/blue-splatter-3.jpg'
-							title='Custom Sunglasses'
-							content='I refurbish, repair, and customize Oakley sunglasses. 
-								I enjoy providing custom paint jobs with a splash of colors - 
-								marbled, splattered, and dripped. Custom paint can be one color, 
-								or a combination of complimentary colors. Patterns can also be small or large, 
-								thin or thick, dense or sparse.  As the customer, you choose!'
-							layout='vertical' />
-						<Callout
-							url='/photography' 
-							img='https://farm6.staticflickr.com/5682/21652998256_7c5d0ce495_b.jpg'
-							title='Stock Photography'
-							content='I have converted my passion of Landscape, Macro, and Travel Photography 
-								into a budding Stock Photography Portfolio.  Check out my work on 
-								iStockPhoto or ShutterStock, or see my books on Blurb or Shutterfly. '
-							layout='vertical' />
-						{/* <Callout
-							url='/homedesign' 
-							img='https://farm66.static.flickr.com/65535/54513221006_5d2117cdf3_b.jpg'
-							title='Home Design & Remodeling'
-							content='We design a holistic plan for interior and exterior design of homes,
-								including kitchens, bathrooms, bedrooms, color schemes, artwork, garages, patios, decks, and more.'
-							layout='vertical' /> */}
+							content='Pixelated offers a wide variety of products and services, 
+							including custom web application development for small businesses, 
+							responsive design, search engine optimization, website performance tuning, 
+							content management systems, accessibility compliance, social media marketing, 
+							e-commerce solutions, accessibility compliance, 
+							integration with small business finance systems, cloud hosting and deployment, 
+							and more. 
+							We specialize in creating custom solutions tailored to your business needs, 
+							ensuring a seamless user experience across all devices and platforms.
+							Explore our detailed offerings below.'
+							layout='horizontal' />
 					</div>
 				</div>
 			</section>
@@ -68,9 +63,7 @@ export default function Home() {
 						<div className="gridItem">
 							<div className="callout-body">
 								Links to each of my social media accounts and a few recent postings from each,
-								including 500px, Ancestry, my Blog, Blurb, eBay, Etsy, Facebook, Flickr, Goodreads,
-								Instagram, iStock Photo, LinkedIn, Pinterest, Reddit, Shutterfly, Shutterstock, 
-								Twitter, Tumblr, Youtube, and more.
+								including my Blog, Facebook, Instagram, LinkedIn, Twitter, and more.
 								Check out the use of RSS feeds and APIs to generate dynamic cards on the page
 								using my React component library.
 								<br/>
@@ -83,8 +76,20 @@ export default function Home() {
 
 			<section id="spotlight-section">
 				<div className="section-container">
+					<br />
+					<CalloutHeader title="Work Portfolio Gallery" />
+					<Carousel 
+						cards={flickrCards} 
+						draggable={true}
+						imgFit="contain" />
+				</div>
+			</section>
+
+			<section id="spotlight-section">
+				<div className="section-container">
 					<CalloutHeader title="Pixelated - Featured Spotlights" />
 					<div className="row-4col">
+						<div className="gridItem" />
 						<div className="gridItem">
 							<Callout
 								img='images/brianwhaley-headshot.jpg'
@@ -94,34 +99,19 @@ export default function Home() {
 									Landscape and Macro Photographer, Avid World Traveler,
 									Advanced Open Water Scuba Diver, Enthusiast of Home-Cooked Food'
 								layout='vertical' />
-						</div><div className="gridItem">
-							<Callout
-								url='photogallery'
-								img='https://farm1.static.flickr.com/736/32093760946_27cb5b34fa_b.jpg'
-								title='Photo Gallery'
-								layout='vertical'
-								content='A carousel of some of my best photographs throughout my life.
-									They are primarily landscape photographs, macro photographs, and travel photographs.
-									The carousel was custom built using React and the Flickr API.
-									You can click on the left or right arrows to navigate, or swipe on a touch enabled device.'/>
-						</div><div className="gridItem">
-							<Callout
-								url='https://www.ebay.com/usr/btw73/'
-								img='images/logos/ebay-logo.png'
-								title='Sunglasses on eBay'
-								content='View some cutomized Oakley sunglasses available for purchase on eBay.'
-								layout='vertical' />
-						</div><div className="gridItem">
-							<Callout
-								url='https://www.goodreads.com/review/list/49377228-brian-whaley?shelf=books-for-work' 
-								img='/images/logos/goodreads-logo.png'
-								title='Goodreads'
-								layout='vertical' 
-								shape='round' 
-								content='This Goodreads Shelf is a collection of books that i have found to be 
-									invaluable to build, strengthen, and manage my career and leadership skills over my career, 
-									i hope these are useful to you too. '/>
 						</div>
+						<div className="gridItem">
+							<Callout
+								url='schedule'
+								img='/images/icons/calendar-icon.jpg'
+								title='Schedule an Appointment'
+								layout='vertical'
+								content='Schedule a consultation appointment with Pixelated 
+								for custom IT development work - web development, 
+								social media marketing, search engine optimization, 
+								and small business modernization.'/>
+						</div>
+						<div className="gridItem" />
 					</div>
 				</div>
 			</section>
--- src/app/(pages)/socialmedia/page.tsx
+++ src/app/(pages)/socialmedia/page.tsx
@@ -12,29 +12,29 @@ ERRORS: 500px, shutterfly - malformed XML RSS
 */
 
 const myRSSFeeds = {
-	SOOpx: { url: "https://500px.com/brianwhaley/rss" },
+	// SOOpx: { url: "https://500px.com/brianwhaley/rss" },
 	blog: { url: "https://blog.pixelated.tech/feed/", iconSrcAlt: "Pixelated Views Blog Post" },
 	/* BLURB */
-	ebay: { url: "https://rssbay.net/feed?keyword=sunglasses&globalId=EBAY-US&auction=1&buyitnow=1&condition=-&seller=btw73&time-frame-type=-&time-frame-value=-" },
-	etsy: { url: "https://www.etsy.com/people/bwhaley73/favorites/items.rss" },
+	// ebay: { url: "https://rssbay.net/feed?keyword=sunglasses&globalId=EBAY-US&auction=1&buyitnow=1&condition=-&seller=btw73&time-frame-type=-&time-frame-value=-" },
+	// etsy: { url: "https://www.etsy.com/people/bwhaley73/favorites/items.rss" },
 	/* FACEBOOK */
-	flickr: { url : "https://www.flickr.com/services/feeds/photoset.gne?nsid=15473210@N04&set=72157712416706518&lang=en-us&format=rss2" },
+	// flickr: { url : "https://www.flickr.com/services/feeds/photoset.gne?nsid=15473210@N04&set=72157712416706518&lang=en-us&format=rss2" },
 	/* foursquare: { url: 'https://feeds.foursquare.com/history/LZSXBIJMSBHI5EQXV1GTQOVQW5XRJ0FP.rss' }, */
 	github: { url: "https://github.com/brianwhaley.atom" },
-	goodreads: { url:  "https://www.goodreads.com/review/list_rss/49377228?key=3KvDb_dRiJMRnHlEeNtUOsn-Ry396qlabqubyfD6jFiSDk9q&shelf=currently-reading" },
+	// goodreads: { url:  "https://www.goodreads.com/review/list_rss/49377228?key=3KvDb_dRiJMRnHlEeNtUOsn-Ry396qlabqubyfD6jFiSDk9q&shelf=currently-reading" },
 	goodreads2: { url: "https://www.goodreads.com/review/list_rss/49377228?key=3KvDb_dRiJMRnHlEeNtUOsn-Ry396qlabqubyfD6jFiSDk9q&shelf=books-for-work", iconSrcAlt: 'Goodreads Book For Work' },
 	/* INSTAGRAM */
 	/* iStockPhoto */
 	/* LinkedIn */
-	pinterest: { url: "https://www.pinterest.com/brianwhaley/feed.rss" },
-	reddit: { url: "https://www.reddit.com/user/btw-73/saved.rss?feed=fc1adaa9f977a389504453447e7c0e6ef68f7037&user=btw-73" },
+	// pinterest: { url: "https://www.pinterest.com/brianwhaley/feed.rss" },
+	// reddit: { url: "https://www.reddit.com/user/btw-73/saved.rss?feed=fc1adaa9f977a389504453447e7c0e6ef68f7037&user=btw-73" },
 	/* https://ssl.reddit.com/prefs/feeds/ */
-	shutterfly: { url: "https://cmd.shutterfly.com/commands/format/rss?site=brianwhaley&page=brianwhaley" },
+	// shutterfly: { url: "https://cmd.shutterfly.com/commands/format/rss?site=brianwhaley&page=brianwhaley" },
 	/* snapchat */
 	/* ShutterStock */
 	/* TickTock */
-	tumblr: { url: "https://pixelatedviews.tumblr.com/rss" },
-	x: { url : "https://rsshub.app/twitter/user/brianwhaley", iconSrcAlt: 'Tweets from Brian Whaley'},
+	// tumblr: { url: "https://pixelatedviews.tumblr.com/rss" },
+	// x: { url : "https://rsshub.app/twitter/user/brianwhaley", iconSrcAlt: 'Tweets from Brian Whaley'},
 	// twitter: { url: "https://twitrss.me/twitter_user_to_rss/?user=brianwhaley" },
 	// twitter: { url : "https://rss.app/feeds/MPULoVcijU5AAHxU.xml", iconSrcAlt: 'Tweets from Brian Whaley'},
 	// twitter: { url: "https://twitrss.me/twitter_user_to_rss/?user=pixelatedviews" }
