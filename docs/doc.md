4e17a8ed6555d79b5f32fcbe49acc6844259823b
Brian Whaley <brian.whaley@gmail.com>
2025-10-12 22:58:20 -0400

    remove shopping cart from header

--- docs/doc.md
+++ docs/doc.md
@@ -50,7 +50,7 @@ npm version minor
 eslint --fix
 npm version patch --force
 git add * -v
-git commit -m "implement microanimations"
+git commit -m "remove shopping cart from header"
 git push pixelated dev --tags
 git push pixelated dev:main
 
--- src/app/elements/header.tsx
+++ src/app/elements/header.tsx
@@ -4,16 +4,12 @@ import React from "react";
 
 // import { MenuAccordionButton } from "../components/menu/pixelated.menu-accordion";
 import { MenuAccordionButton } from "@brianwhaley/pixelated-components";
-import { CartButton } from "@brianwhaley/pixelated-components";
 
 export default function Header() {
 	return (
 		<div className="section-container">
 			<MenuAccordionButton />
 			<h2 className="pull-left pad textOutline">Pixelated Technologies</h2>
-			<div className="right">
-				<CartButton href={'/cart'} />
-			</div>
 		</div>
 	);
 }
