b0d760813c1659aa5ec7e3d1cc7cef8aa268bc37
Brian Whaley <brian.whaley@gmail.com>
2025-11-20 14:20:26 -0500

    expose contentful env vars in next.config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,11 @@ const nextConfig: NextConfig = {
 	typescript: {
 		ignoreBuildErrors: true,
 	},
+	env: {
+		CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+		CONTENTFUL_MANAGEMENT_ACCESS_TOKEN: process.env.CONTENTFUL_MANAGEMENT_ACCESS_TOKEN,
+		CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT,
+	},
 
 	async redirects() {
 		return [
