eedb3b9dbaa31da8f976278dda4d2ceb047d43f5
Brian Whaley <brian.whaley@gmail.com>
2025-11-21 01:47:51 -0500

    more SmartImage + callout performance fixes

--- docs/doc.md
+++ docs/doc.md
@@ -58,7 +58,7 @@ echo "Updating packages..." && npm outdated | awk 'NR>1 {print $1"@"$4}' | while
 npm audit fix --force
 npm version patch --force
 git add * -v
-git commit -m "pixelated-components bump for SmartImage performance fixes"
+git commit -m "more SmartImage + callout performance fixes"
 git push -u pixelated dev --tags
 git push pixelated dev:main
 
--- src/app/elements/socialtags.tsx
+++ src/app/elements/socialtags.tsx
@@ -16,17 +16,17 @@ export default function SocialTags() {
 			</div>
 
 			<div className="row-11col">
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="http://blog.pixelated.tech" img="/images/icons/blog-icon.jpg" imgAlt="Pixelated Technologies Blog" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://share.google/an1Yqe6CTFA946zZV" img="/images/logos/google-business.png" imgAlt="Google Business" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://www.linkedin.com/company/106825397/" img="/images/logos/linkedin-logo.png" imgAlt="LinkedIn" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://www.reddit.com/r/PixelatedTech/" img="/images/logos/reddit-logo.png" imgAlt="Reddit" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://www.facebook.com/profile.php?id=61577216017129" img="/images/logos/facebook-logo.png" imgAlt="Facebook" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://www.instagram.com/_pixelatedtech_/" img="/images/logos/instagram-logo.jpg" imgAlt="Instagram" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="http://twitter.com/pixelatedviews" img="/images/logos/twitter-logo.png" imgAlt="Twitter" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://www.yelp.com/user_details?userid=SUFlzBCRMR0OIc3D3nN5pg" img="/images/logos/yelp-logo.png" imgAlt="Yelp" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://www.tumblr.com/pixelatedtech" img="/images/logos/tumblr-logo.png" imgAlt="Tumblr" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://patch.com/new-jersey/denville-nj/business/listing/558828/pixelated-technologies" img="/images/logos/patch-logo.png" imgAlt="Patch" /></div>
-				<div className="gridItem"><Callout style='full' imgShape="squircle" url="https://www.eventbrite.com/o/brian-whaley-120645314676" img="/images/logos/eventbrite-logo.png" imgAlt="EventBrite" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="http://blog.pixelated.tech" img="/images/icons/blog-icon.jpg" imgAlt="Pixelated Technologies Blog" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://share.google/an1Yqe6CTFA946zZV" img="/images/logos/google-business.png" imgAlt="Google Business" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://www.linkedin.com/company/106825397/" img="/images/logos/linkedin-logo.png" imgAlt="LinkedIn" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://www.reddit.com/r/PixelatedTech/" img="/images/logos/reddit-logo.png" imgAlt="Reddit" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://www.facebook.com/profile.php?id=61577216017129" img="/images/logos/facebook-logo.png" imgAlt="Facebook" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://www.instagram.com/_pixelatedtech_/" img="/images/logos/instagram-logo.jpg" imgAlt="Instagram" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="http://twitter.com/pixelatedviews" img="/images/logos/twitter-logo.png" imgAlt="Twitter" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://www.yelp.com/user_details?userid=SUFlzBCRMR0OIc3D3nN5pg" img="/images/logos/yelp-logo.png" imgAlt="Yelp" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://www.tumblr.com/pixelatedtech" img="/images/logos/tumblr-logo.png" imgAlt="Tumblr" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://patch.com/new-jersey/denville-nj/business/listing/558828/pixelated-technologies" img="/images/logos/patch-logo.png" imgAlt="Patch" /></div>
+				<div className="gridItem"><Callout style='full' imgShape="squircle" cloudinaryEnv="dlbon7tpq" url="https://www.eventbrite.com/o/brian-whaley-120645314676" img="/images/logos/eventbrite-logo.png" imgAlt="EventBrite" /></div>
 			</div>
         
 		</>
