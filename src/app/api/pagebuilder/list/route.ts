520c76dec35a199041ba3bbce07ea30e60a46c2e
Brian Whaley <brian.whaley@gmail.com>
2025-11-22 20:16:32 -0500

    more SmartImage + callout performance fixes

--- src/app/api/pagebuilder/list/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { listPages } from '@brianwhaley/pixelated-components/server';
-
-export async function GET() {
-	const result = await listPages();
-	return NextResponse.json(result);
-}
