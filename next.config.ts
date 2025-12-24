7d11c890eae0673483979595eebc3cd8ee84e9a4
Brian Whaley <brian.whaley@gmail.com>
2025-12-23 20:09:41 -0500

    use client fix for 404 page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	experimental: {
-    	optimizeCss: true,
+    	optimizeCss: false,
   	},
 	transpilePackages: ['@pixelated-tech/components'],
 	trailingSlash: false,
